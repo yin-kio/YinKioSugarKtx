1fd6e90c1f68bb6e6af291c265cb4c009ee84372
Igor Kim <jamycake@hotmail.com>
2022-02-15 13:32:48 +0300

    add coroutines wrapper for Fragment and Activity

--- app/src/main/java/com/yinkio/shugar_ktx/Coroutines.kt
+++ app/src/main/java/com/yinkio/shugar_ktx/Coroutines.kt
@@ -1,29 +1,49 @@
 package com.yinkio.shugar_ktx
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.coroutineScope
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 
-inline fun ViewModel.onIO(crossinline action: suspend CoroutineScope.() -> Unit) : Job{
+inline fun AppCompatActivity.onIO(crossinline action: suspend CoroutineScope.() -> Unit): Job {
+    return lifecycle.coroutineScope.launch(Dispatchers.IO) { action() }
+}
+
+inline fun Fragment.onIO(crossinline action: suspend CoroutineScope.() -> Unit): Job {
+    return lifecycle.coroutineScope.launch(Dispatchers.IO) { action() }
+}
+
+
+inline fun ViewModel.onIO(crossinline action: suspend CoroutineScope.() -> Unit): Job {
     return viewModelScope.onIO(action)
 }
 
-inline fun CoroutineScope.onIO(crossinline action: suspend CoroutineScope.() -> Unit) : Job{
+inline fun CoroutineScope.onIO(crossinline action: suspend CoroutineScope.() -> Unit): Job {
     return launch(Dispatchers.IO) {
         action()
     }
 }
 
-suspend inline fun CoroutineScope.onMain(crossinline action: CoroutineScope.() -> Unit){
-    withContext(Dispatchers.Main){action()}
+suspend inline fun CoroutineScope.onMain(crossinline action: CoroutineScope.() -> Unit) {
+    withContext(Dispatchers.Main) { action() }
 }
 
-inline fun ViewModel.onDefault(crossinline action: suspend CoroutineScope.() -> Unit) : Job{
+inline fun ViewModel.onDefault(crossinline action: suspend CoroutineScope.() -> Unit): Job {
     return viewModelScope.onDefault(action)
 }
 
-inline fun CoroutineScope.onDefault(crossinline action: suspend CoroutineScope.() -> Unit) : Job{
+inline fun CoroutineScope.onDefault(crossinline action: suspend CoroutineScope.() -> Unit): Job {
     return launch(Dispatchers.Default) {
         action()
     }
+}
+
+inline fun Fragment.onDefault(crossinline action: suspend CoroutineScope.() -> Unit): Job {
+    return lifecycle.coroutineScope.launch(Dispatchers.Default) { action() }
+}
+
+inline fun AppCompatActivity.onDefault(crossinline action: suspend CoroutineScope.() -> Unit): Job {
+    return lifecycle.coroutineScope.launch(Dispatchers.Default) { action() }
 }
---
+++ app/src/main/java/com/yinkio/shugar_ktx/Strings.kt
@@ -0,0 +1,6 @@
+package com.yinkio.shugar_ktx
+
+import android.text.Html
+import android.text.Spanned
+import androidx.fragment.app.Fragment
+
